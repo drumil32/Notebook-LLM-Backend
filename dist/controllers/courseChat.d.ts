f43a39da3c3b41d7b8920b16f599c9c9e623c204
drumil32 <akheniad@gmail.com>
2025-08-24 16:09:51 +0530

    [config]: .gitignore update

--- dist/controllers/courseChat.d.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from 'express';
-export declare class CourseChatController {
-    private readonly CHAT_HISTORY_TTL;
-    private readonly embeddings;
-    constructor();
-    chat(req: Request, res: Response): Promise<void>;
-}
-export declare const courseChatController: CourseChatController;
-//# sourceMappingURL=courseChat.d.ts.map
--- dist/routes/courseChat.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const rateLimiter_1 = require("../middleware/rateLimiter");
-const courseChat_1 = require("../controllers/courseChat");
-const router = (0, express_1.Router)();
-router.post('/', rateLimiter_1.courseChatRateLimit, courseChat_1.courseChatController.chat.bind(courseChat_1.courseChatController));
-exports.default = router;
-//# sourceMappingURL=courseChat.js.map
