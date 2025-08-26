1332c8b845be716d57611451a867ea582cc8dbb2
drumil32 <akheniad@gmail.com>
2025-08-26 08:27:42 +0530

    [config]: update .gitignore and add readme.md

--- dist/middleware/origin-validation.d.ts
+++
@@ -1,4 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-export declare const getClientIp: (req: Request) => string;
-export declare const validateOrigin: (req: Request, res: Response, next: NextFunction) => void | Response<any, Record<string, any>>;
-//# sourceMappingURL=origin-validation.d.ts.map
